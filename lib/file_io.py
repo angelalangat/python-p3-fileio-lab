c1553be57b684ac16f2f676b3cee96bd77bc5443
angie <angelaclangat@gmai.com>
2024-09-27 03:13:11 +0300

    Updates file_io.py

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = f"{file_name}.txt"  # Add .txt extension
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = f"{file_name}.txt"  # Add .txt extension
+    with open(file_name, 'a') as file:
+        file.write(append_content)  # Changed file_content to append_content
 
 def read_file(file_name):
-    pass
+    file_name = f"{file_name}.txt"  # Add .txt extension
+    with open(file_name, 'r') as file:
+        return file.read()
